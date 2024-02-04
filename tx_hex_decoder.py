46cfb4d3a07734a0ee222af14d5853976417e206
Otaiki1 <otaikisadiq2804@gmail.com>
2024-02-04 11:41:21 +0100

    refac: modify script for segwit , add test

--- tx_hex_decoder.py
+++ tx_hex_decoder.py
@@ -1,63 +1,68 @@
-# store the transaction as bytes 
-raw_tx = bytes.fromhex('0100000001c997a5e56e104102fa209c6a852dd90660a20b2d9c352423edce25857fcd3704000000004847304402204e45e16932b8af514961a1d3a1a25fdf3f4f7732e9d624c6c61548ab5fb8cd410220181522ec8eca07de4860a4acdd12909d831cc56cbbac4622082221a8768d1d0901ffffffff0200ca9a3b00000000434104ae1a62fe09c5f51b13905f07f06b99a2f7159b2225f374cd378d71302fa28414e7aab37397f554a7df5f142c21c1b7303b8a0626f1baded5c72a704f7e6cd84cac00286bee0000000043410411db93e1dcdb8a016b49840f8c53bc1eb68a382e97b1482ecad7b148a6909a5cb2e0eaddfb84ccf9744464f82e160bfa9b8b64f9d4c03f999b8643f656b412a3ac00000000')
+def decode_transaction(raw_tx):
+     # Convert the raw transaction to bytes if it's not already
+    if isinstance(raw_tx, str):
+        raw_tx = bytes.fromhex(raw_tx)
 
-# import hashlib for sha256
-import hashlib
+    # Version
+    version = raw_tx[0:4][::-1].hex()
+    print("version: ", version)
 
-# first round of sha256
-hash1 = hashlib.sha256(raw_tx).digest()
+    # Marker and flag (only present in SegWit transactions)
+    marker = raw_tx[4]
+    flag = raw_tx[5]
+    print("marker: ", marker)
+    print("flag: ", flag)
 
-# second round of sha256 gives us the txid
-hash2 = hashlib.sha256(hash1).digest()
+    # Number of inputs
+    num_inputs = raw_tx[6]
+    print("num_inputs: ", num_inputs)
 
-print("Two rounds of SHA256 on the raw tx gives us: ", hash2.hex())
+    # Inputs
+    inputs = []
+    for i in range(num_inputs):
+        offset = 7 + i*41
+        input = raw_tx[offset:offset+41]
+        inputs.append(input.hex())
+    print("inputs: ", inputs)
 
-# We can use the python shorthand '[::-1]' to reverse the bytes, giving us the output in little endian notation
-txid = hash2[::-1]
-print("Reversing the bytes to little endian: ", txid.hex())
+    # Number of outputs
+    num_outputs_offset = 7 + num_inputs*41
+    num_outputs = raw_tx[num_outputs_offset]
+    print("num_outputs: ", num_outputs)
 
-# Transaction size
-size = len(raw_tx)
-print("size: ", size)
+    # Outputs
+    outputs = []
+    for i in range(num_outputs):
+        offset = num_outputs_offset + 1 + i*33
+        output = raw_tx[offset:offset+33]
+        outputs.append(output.hex())
+    print("outputs: ", outputs)
 
-# Transaction weight
-weight = size*4
-print("weight: ", weight)
+    # Witness data (only present in SegWit transactions)
+    witness_offset = num_outputs_offset + 1 + num_outputs*33
+    witness = raw_tx[witness_offset:-4]
+    print("witness: ", witness.hex())
 
-# Virtual size
-import math
-vsize = math.ceil(weight/4) # Note that vsize/vbytes will round up
-print("vsize: ", vsize)
+    # Locktime
+    locktime = raw_tx[-4:][::-1].hex()
+    print("locktime: ", locktime)
+    
+    return(version, num_inputs, inputs, num_outputs, outputs, locktime)
 
-# Version
-version = raw_tx[0:4][::-1].hex()
-print("version: ", int(version, 16))
 
-# Number of inputs
-num_inputs = raw_tx[4]
-print("num_inputs: ", num_inputs)
+def test_decode_transaction():
+    raw_tx = '020000000001010ccc140e766b5dbc884ea2d780c5e91e4eb77597ae64288a42575228b79e234900000000000000000002bd37060000000000225120245091249f4f29d30820e5f36e1e5d477dc3386144220bd6f35839e94de4b9cae81c00000000000016001416d31d7632aa17b3b316b813c0a3177f5b6150200140838a1f0f1ee607b54abf0a3f55792f6f8d09c3eb7a9fa46cd4976f2137ca2e3f4a901e314e1b827c3332d7e1865ffe1d7ff5f5d7576a9000f354487a09de44cd00000000'
+    # Call your decode_transaction function
+    (version, num_inputs, inputs, num_outputs, outputs, locktime) = decode_transaction(raw_tx)
+    print(version)
 
-# Inputs
-inputs = []
-for i in range(num_inputs):
-    offset = 5 + i*41
-    input = raw_tx[offset:offset+41]
-    inputs.append(input.hex())
-print("inputs: ", inputs)
+    # Assert that the outputs are as expected
+    assert version == '00000002', f'Expected version to be 00000002, but got {version}'
+    assert num_inputs == 1, f'Expected num_inputs to be 1, but got {num_inputs}'
+    assert len(inputs) == num_inputs, f'Expected {num_inputs} inputs, but got {len(inputs)}'
+    assert num_outputs == 2, f'Expected num_outputs to be 2, but got {num_outputs}'
+    assert len(outputs) == num_outputs, f'Expected {num_outputs} outputs, but got {len(outputs)}'
+    assert locktime == '00000000', f'Expected locktime to be 00000000, but got {locktime}'
 
-# Number of outputs
-num_outputs_offset = 5 + num_inputs*41
-num_outputs = raw_tx[num_outputs_offset]
-print("num_outputs: ", num_outputs)
-
-# Outputs
-outputs = []
-for i in range(num_outputs):
-    offset = num_outputs_offset + 1 + i*33
-    output = raw_tx[offset:offset+33]
-    outputs.append(output.hex())
-print("outputs: ", outputs)
-
-# Locktime
-locktime = raw_tx[-4:][::-1].hex()
-print("locktime: ", int(locktime, 16))
+# Call the test function
+test_decode_transaction()
